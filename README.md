35a833896495fee639a1e17821b2e98409d3520e
toebgen <tobias.roth.tr@gmail.com>
2016-10-15 12:13:03 -0700

    Flask hello world app.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cyberfoosball
 Digital Foosball Table
+
+Checkout the [Wiki](https://github.com/toebgen/cyberfoosball/wiki)!
---
+++ visualizer/app/models.py
@@ -0,0 +1,53 @@
+from app import db
+
+# http://flask-sqlalchemy.pocoo.org/2.1/models/
+# Many-to-Many Relationships for user and games
+games = db.Table('games',
+                 db.Column('game_id', db.Integer, db.ForeignKey('game.id')),
+                 db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+                 )
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hash = db.Column(db.Integer, index=True, unique=True)
+    name = db.Column(db.String(64), index=True, unique=False)
+    email = db.Column(db.String(120), index=True, unique=True)
+    games = db.relationship('Game', secondary=games,
+                            backref=db.backref('games', lazy='dynamic'))
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)
+        except NameError:
+            return str(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime)
+    body = db.Column(db.String(140))
+    home_defense = db.Column(db.Integer, db.ForeignKey('user.id'))
+    home_offense = db.Column(db.Integer, db.ForeignKey('user.id'))
+    guest_defense = db.Column(db.Integer, db.ForeignKey('user.id'))
+    guest_offense = db.Column(db.Integer, db.ForeignKey('user.id'))
+    home_goals = db.Column(db.Integer, unique=False)
+    guest_goals = db.Column(db.Integer, unique=False)
+
+    def __repr__(self):
+        return '<Game %r>' % (self.body)
