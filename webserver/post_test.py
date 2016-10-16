5c7146df29aaf24050e9ddf82fc24fbbd8e29213
toebgen <tobias.roth.tr@gmail.com>
2016-10-16 16:55:56 -0700

    Use correct json syntax

--- webserver/post_test.py
+++ webserver/post_test.py
@@ -4,12 +4,12 @@ import json
 
 url = "http://localhost:5000/status_update"
 data = {
-    'home_defense': 'hash1',
-    'home_offense': 'hash2',
-    'guest_defense': 'hash3',
-    'guest_offense': 'hash4',
-    'home_goals': 2,
-    'guest_goals': 1
+    "home_defense": "hash1",
+    "home_offense": "hash2",
+    "guest_defense": "hash3",
+    "guest_offense": "hash4",
+    "home_goals": 2,
+    "guest_goals": 1
 }
 
 r = requests.post(url, json=data)
